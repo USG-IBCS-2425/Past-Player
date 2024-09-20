39af674b4bc316ef26b2eb08c3930b30a419c409
Yangqing (Vera) Li <yangqing_li@m018740.aw.org>
2024-09-20 13:52:01 -0700

    zoo project

---
+++ Deer.java
@@ -0,0 +1,29 @@
+class Deer{
+	String name;
+	int age;
+	String mbti;
+	String gender;
+
+
+	public String getName(){
+		return name;
+	}
+	public int getAge(){
+		return age;
+	}
+	public String getMBTI(){
+		return mbti;
+	}
+	public String getGender(){
+		return gender;
+	}
+
+	public Deer(String n, int a, String m, String g){
+		name = n;
+		age = a;
+		mbti = m;
+		gender = g;
+
+	}
+
+}
